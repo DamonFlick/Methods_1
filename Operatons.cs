e48d2e80c0d73f736dda9c59ddbedd07d8c7a5dd
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-02-22 17:04:11 -0800

    Add project files.

---
+++ Operatons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_1
+{
+    class Operations
+    { 
+        public static int addTwo(int x)
+        {
+            int result = x + 2;
+            return result;
+        }
+
+        public static int multTwo(int x)
+        {
+            int result = x * 2;
+            return result;
+        }
+
+        public static int divTwo(int x)
+        {
+            int result = x / 2;
+            return result;
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number, and I'll add two to it! \n");
+            Console.WriteLine("\n" + Operations.addTwo(Convert.ToInt32(Console.ReadLine())));
+
+
+            Console.WriteLine("Enter a number, and I'll multiply it by two! \n ");
+            Console.WriteLine("\n" + Operations.multTwo(Convert.ToInt32(Console.ReadLine())));
+           
+
+            Console.WriteLine("Enter a number, and I'll divide it by two! \n ");
+            Console.WriteLine("\n" + Operations.divTwo( Convert.ToInt32(Console.ReadLine())));
+            Console.ReadLine();
+        }
+    }
+}
